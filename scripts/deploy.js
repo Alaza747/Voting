994150b12a7f931b41964591d33f86840b458fe4
Anton Laza <anton.laza.0116@gmail.com>
2023-02-16 20:38:53 +0100

    check castVote()

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,22 +13,24 @@ async function main() {
 
   await vote.deployed();
 
-  console.log(`Deployed to ${vote.address}`);
+  // console.log(`Deployed to ${vote.address}`);
 
   await vote.createVote("Favourite Number?", [0,1,2,3,4,5,6,7,8,9,10])
   let result = await vote.votes(1);
-  console.log("New Vote added ID[%i] : %s", await vote.idVote(), result);
+  // console.log("New Vote added ID[%i] : %s", await vote.idVote(), result);
 
 
   await vote.createVote("Favourite Color?", [1, 2])
   result = await vote.votes(2);
-  console.log("New Vote added ID[%i] : %s", await vote.idVote(), result);
+  // console.log("New Vote added ID[%i] : %s", await vote.idVote(), result);
 
   result = await vote.getVote(1);
-  console.log("Vote question: ", result.voteQuestion);
-  for(i = 0; i < result.voteOptions.length; i++) {
-    console.log("Answer option %i: %i", i, result.voteOptions[i].toNumber());
-  }
+  // console.log("Vote question: ", result.voteQuestion);  
+  // for(i = 0; i < result.voteOptions.length; i++) {
+  //   console.log("Answer option %i: %i", i, result.voteOptions[i].toNumber());
+  // }
+
+  await vote.castVote(1, 3);
 
 }
 
