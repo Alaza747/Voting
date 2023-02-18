716eb77abe93f2f7fae7f98380b7a1ebff2e767e
Anton Laza <anton.laza.0116@gmail.com>
2023-02-18 16:59:40 +0100

    clean code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,3 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
@@ -13,43 +7,43 @@ async function main() {
 
   await vote.deployed();
 
-// ----------------------------------------------------------------------------------------------------  
+  // ----------------------------------------------------------------------------------------------------  
   // Deployment
   console.log(`Deployed to ${vote.address}`);
 
-// ----------------------------------------------------------------------------------------------------
+  // ----------------------------------------------------------------------------------------------------
   // Create Votes
-  await vote.createVote("Favourite Number?", ["0","1","2","3","4","5","6","7","8","9","10"])
+  await vote.createVote("Favourite Number?", ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
   await vote.createVote("Favourite Color?", ["white", "black", "brown", "red", "orange", "yellow", "green", "blue",])
   await vote.createVote("Favourite Word?", ["abc", "def", "ghi"])
-  
+
+  // ----------------------------------------------------------------------------------------------------
   // Cast a vote
   await vote.castVote(1, 3);
 
-  
+  // ----------------------------------------------------------------------------------------------------
   // Get a Vote
   result = await vote.getVote(3);
   console.log("Vote question: ", result.voteQuestion);
-  if(result.openStatus === true) {
+  if (result.openStatus === true) {
     console.log("Vote status: open");
   } else {
-    console.log("Vote status: closed");  
+    console.log("Vote status: closed");
   }
-  for(i = 0; i < result.voteOptions.length; i++) {
+  for (i = 0; i < result.voteOptions.length; i++) {
     console.log("Answer option %i: %s", i, result.voteOptions[i]);
   }
 
+  // ----------------------------------------------------------------------------------------------------
   // Close a Vote
   await vote.closeVote(3);
   result = await vote.getVote(3);
   console.log("Vote question: ", result.voteQuestion);
-  if(result.openStatus === true) {
+  if (result.openStatus === true) {
     console.log("Vote status: open");
   } else {
-    console.log("Vote status: closed");  
+    console.log("Vote status: closed");
   }
-
-
 }
 
 // We recommend this pattern to be able to use async/await everywhere
